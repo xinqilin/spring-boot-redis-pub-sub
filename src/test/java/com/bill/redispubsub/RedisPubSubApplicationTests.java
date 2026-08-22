9502b2b23a3d72f846a0a905d5b7a93fe675e092
bill.lin <zzx123bill@gmail.com>
2026-08-22 22:16:30 +0800

    setup project

---
+++ src/test/java/com/bill/redispubsub/RedisPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.redispubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPubSubApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
